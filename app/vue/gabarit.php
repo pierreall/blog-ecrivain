860429b895fe4af71e4896cd358c2db0cf584f16
pierreall <alloinp@gmail.com>
2017-10-14 18:34:15 +0200

    ajout fichier gabarit.php

---
+++ app/vue/gabarit.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+
+</header>
+<section class="container">
+ <?php $contenu ?>
+</section>
+ <footer>
+
+</footer>
+</body>
+</html>
