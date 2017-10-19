e9738a0f1a666c9152eecfcd6daeaedca486280e
pierreall <alloinp@gmail.com>
2017-10-19 17:25:10 +0200

    affichage d un billet

--- app/controler/BilletControler.php
+++ app/controler/BilletControler.php
@@ -2,8 +2,6 @@
 
 namespace App\Controler;
 use App\Model\DAO\BilletDAO;
-use  App\Vue\BilletAffichageVue;
-
 
 
 class BilletControler
@@ -25,15 +23,14 @@ class BilletControler
 
     public function affichage($id_post){
         if(isset($id_post)){
-//            var_dump($this->billet->read($id_post));
-ob_start();
+
+            $donneeBilletRead = $this->billet->read($id_post);
+
+
+//            ob_start();
             include 'app/vue/BilletAffichageVue.php';
-            ob_end_flush();
+//            $contenu = ob_end_flush();
         }
-
-        echo 'appel de la méthode '. __METHOD__ .' <br> paramètre = '.$id_post;
-
-
 
     }
 
--- app/vue/BilletAffichageVue.php
+++ app/vue/BilletAffichageVue.php
@@ -1,28 +1,23 @@
-<?php
-//namespace App\Vue\BilletAffichageVue;
-//
-//use App\Controler\BilletControler;
-//use App\Model\DAO\BilletDAO;
-//?>
-
+<?php ob_start();
+?>
 <div class="row">
     <div class="col-lg-12">
         <h2 class="titre">
-            <?php //echo $donneeBilletRead['titre']  ; ?>
+            <?php echo $donneeBilletRead['titre'] ; ?>
         </h2>
     </div>
 </div>
     <div class="row">
         <div class="col-lg-12">
             <p class="contenu">
-                <?php //echo $donneeBilletRead['contenu']; ?>
+                <?php echo $donneeBilletRead['contenu']; ?>
             </p>
         </div>
     </div>
 <div class="row">
     <div class="col-lg-12">
         <auteur>
-            <?php //echo $donneeBilletRead['auteur']; ?>
+            <?php echo $donneeBilletRead['auteur']; ?>
         </auteur>
     </div>
 </div>
@@ -33,8 +28,8 @@
         </date>
     </div>
 </div>
-
-
-<?php //include 'gabarit.php'; ?>
+<?php $contenu = ob_end_flush()?>
+<?php $title = 'Le titre de la page'; ?>
+<?php include 'gabarit.php'; ?>
 
 
